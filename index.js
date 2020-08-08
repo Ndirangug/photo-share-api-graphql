cc149635e7ac9d885171be4a3b59b7e8d4dad70e
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-09 02:59:05 +0300

    impkemented typedefs and resolvers

--- index.js
+++ index.js
@@ -1,40 +1,91 @@
 // 1. Require 'apollo-server'
 const { ApolloServer } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
 
 const typeDefs = `
+
+scalar DateTime
+
+enum PhotoCategory {
+  SELFIE
+  PORTRAIT
+  ACTION
+  LANDSCAPE
+  GRAPHIC
+}
+
 input photoInput{
   name: String!
   description: String
+  category: PhotoCategory=PORTRAIT
 }
 
-type Photo{
-  
-    name: String!
-    description: String
-  
+type Photo {
+  id: ID!
+  url: String!
+  name: String!
+  description: String
+  category: PhotoCategory!
+  postedBy: User!
+  created: DateTime!
 }
 
+type User {
+  githubLogin: ID!
+  name: String
+  avatar: String
+  postedPhotos: [Photo!]!
+}
+  
 type Query {
   totalPhotos: Int!
+  allPhotos(after: DateTime): [Photo!]!
 }
 
 type Mutation{
-  postPhoto(input: photoInput): Boolean!
+  postPhoto(input: photoInput): Photo!
 }
 `;
 
-var photos = []
-
 const resolvers = {
   Query: {
-    totalPhotos: () => 42,
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
+
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args.input)
-      return true
-    }
-  }
+      var newPhoto = {
+        id: _id++,
+        ...args.input,
+        created: new Date()
+      };
+      photos.push(newPhoto);
+
+      return newPhoto;
+    },
+  },
+
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((u) => u.githubLogin === parent.githubUser);
+    },
+  },
+
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser === parent.githubLogin);
+    },
+  },
+
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: (value) => new Date(value),
+    serialize: (value) => new Date(value).toISOString(),
+    parseLiteral: (ast) => ast.value,
+  }),
 };
 
 const server = new ApolloServer({
@@ -45,3 +96,37 @@ const server = new ApolloServer({
 server
   .listen()
   .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
+
+//sample data
+
+_id = 0
+var users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+var photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+    created: "3-28-1977"
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+    created: "1-2-1985"
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+    created: "2018-04-15T19:09:57.308Z"
+  },
+];
